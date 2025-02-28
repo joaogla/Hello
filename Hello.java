d4ed5b328fcae63dea4444cac7815b1a17945c1f
Joao Guilherme Lemes Argenta <joaoargenta3@gmail.com>
2025-02-28 20:11:49 -0300

    Adicionando Hello.java com método printHello()

---
+++ Hello.java
@@ -0,0 +1,10 @@
+package JoaoGuilherme.Hello;
+
+public class Hello {
+  public static void printHello(){
+    System.err.println("Hello, World!");
+  }
+  public static void main(String[] args){
+    printHello();
+  }
+}
