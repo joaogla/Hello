8bb6d9c9bd3bc9945ae4dd8573218532de087077
Joao Guilherme Lemes Argenta <joaoargenta3@gmail.com>
2025-02-28 20:14:58 -0300

    Adicionando método printOla() que escreve Olá Mundo

--- Hello.java
+++ Hello.java
@@ -4,7 +4,11 @@ public class Hello {
   public static void printHello(){
     System.err.println("Hello, World!");
   }
+  public static void printOla(){
+    System.err.println("Ola, Mundo!");
+  }
   public static void main(String[] args){
     printHello();
+    printOla();
   }
 }
